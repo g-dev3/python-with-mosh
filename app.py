caf08df1896ca52050edb5f5ff00bba427041c08
G Developer <gpkumawat548@gmail.com>
2024-02-27 15:00:37 +0530

    Variable and taking input

---
+++ app.py
@@ -0,0 +1,12 @@
+full_name = 'John Smith'
+age = 20
+
+
+name = input ('What is your name?')
+
+print('Hii',name)
+print(f'hello its me {name}')
+print('hello ' + name)
+
+color = input('What is your favorit Color? ')
+print(f'{name} likes {color}')
