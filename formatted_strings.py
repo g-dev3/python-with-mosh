29250cf95781238285d1cfb3e838496e01677542
G Developer <gpkumawat548@gmail.com>
2024-02-27 15:31:11 +0530

    formatted strings

---
+++ formatted_strings.py
@@ -0,0 +1,13 @@
+first = 'John'
+last = 'Smith'
+# John[Smith] is a coder
+
+massage = first + ' ['+ last+'] is a coder'
+
+print(massage)
+
+# formatted string
+
+msg = f'{first} [{last}] is a coder'
+
+print(msg)
