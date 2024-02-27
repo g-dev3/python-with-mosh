62653817f6386fa248fdac8a24ad33bc1d34dcaa
G Dev3 <gdev3.js@gmail.com>
2024-02-27 21:41:44 +0530

    comparison oprator

---
+++ comparison_oprator.py
@@ -0,0 +1,18 @@
+temperature = 12
+
+if temperature >=30:
+  print("It's a hot day")
+elif temperature <= 10:
+  print("It's a cold day")
+else:
+  print("It's neither hot nor cold")
+
+
+name = 'Gopal'
+
+if len(name) <= 3:
+  print("nem must be at least 3 characters")
+elif len(name)>=50:
+  print("name can be a maximum of 50 characters")
+else:
+  print("name looks good")
