784133f7e4a104d9a6409b98b5bc905396923d3d
G Dev3 <gdev3.js@gmail.com>
2024-02-27 15:47:14 +0530

    String methods

---
+++ string_methods.py
@@ -0,0 +1,23 @@
+Course = 'Python for Begginers'
+
+print(len(Course))
+
+print(Course.upper())
+
+print(Course.lower())
+
+print(Course.find('P'))
+
+print(Course.find('Begginers'))
+
+print(Course.replace('Begginers','Absolute Beginners'))
+
+print(Course)
+
+print('Python' in Course)
+
+
+
+
+
+
