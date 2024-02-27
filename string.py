00836be14a0dbb16b2e5d651c544a9a4d317289e
G Developer <gpkumawat548@gmail.com>
2024-02-27 15:21:54 +0530

    Strings

---
+++ string.py
@@ -0,0 +1,31 @@
+course = 'Python for Beginners'
+
+course_1 = "python's Course for Beginners"
+
+course_2 = 'Python for "Beginners"'
+
+course_3 = ''' 
+Hi John,
+
+Here is our first email to you.
+
+Thank you,
+The support team
+'''
+
+print(course_3)
+
+print(course)
+print(course_1)
+print(course_2)
+
+print(course[1])
+print(course[0])
+print(course[0:3])
+print(course[1:])
+print(course[:5])
+print(course[:])
+
+name = 'Jennifer'
+
+print(name[1:-1])
