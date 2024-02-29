46fd162fe6ef9785f5da704098addeb415a1a5b6
G Dev3 <gdev3.js@gmail.com>
2024-02-29 06:08:32 +0530

    Unpacking

---
+++ unpacking.py
@@ -0,0 +1,16 @@
+coordinates = (1,2,3)
+
+coordinates[0] * coordinates[1] * coordinates[2]
+
+
+x = coordinates[0]
+
+y = coordinates[1]
+
+z = coordinates[2]
+
+x, y, z = coordinates
+
+print(x)
+print(y)
+print(z)
